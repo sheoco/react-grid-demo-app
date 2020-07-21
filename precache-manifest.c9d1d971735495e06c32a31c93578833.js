86056776e3632d5c7e4106e0631e944bf1861ada
Sheo Shankar <sheointime@gmail.com>
2020-07-21 17:44:32 +0530

    Updates

---
+++ precache-manifest.c9d1d971735495e06c32a31c93578833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853a75700d642d6e8f809f08b88be114",
+    "url": "/react-grid-demo-app/index.html"
+  },
+  {
+    "revision": "22b526133272b84f05de",
+    "url": "/react-grid-demo-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b8f9de4df2a556e737dd",
+    "url": "/react-grid-demo-app/static/js/2.a27506f7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-grid-demo-app/static/js/2.a27506f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b526133272b84f05de",
+    "url": "/react-grid-demo-app/static/js/main.c793a1d8.chunk.js"
+  },
+  {
+    "revision": "79d2bc84e90ac65999e7",
+    "url": "/react-grid-demo-app/static/js/runtime-main.9a604578.js"
+  }
+]);
